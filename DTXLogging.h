f639c1166e949dab233cce089e9fbd4595092397
Leo Natan <leo.natan@outlook.com>
2019-05-30 19:04:25 +0300

    Enhance the logging macros to work even when called very early on in process lifetime

--- DTXLogging.h
+++ DTXLogging.h
@@ -22,15 +22,20 @@
 
 #define DTX_CREATE_LOG_PREFIX(name, prefix) static NSString* __current_log_prefix = prefix;\
 static os_log_t __current_file_log;\
-__attribute__((constructor))\
-static void __prepare_log() { \
+__attribute__((__not_tail_called__)) \
+__attribute__((__nothrow__)) \
+__unused static os_log_t __prepare_and_return_file_log (void) { \
+static dispatch_once_t __current_file_log_once_token; \
+dispatch_once(&__current_file_log_once_token, ^{ \
 __current_file_log = os_log_create(DTX_LOG_SUBSYSTEM, #name); \
+}); \
+return __current_file_log; \
 }
 
-#define dtx_log_debug(format, ...) __dtx_log(__current_file_log, OS_LOG_TYPE_DEBUG, __current_log_prefix, format, ##__VA_ARGS__)
-#define dtx_log_info(format, ...) __dtx_log(__current_file_log, OS_LOG_TYPE_INFO, __current_log_prefix, format, ##__VA_ARGS__)
-#define dtx_log_error(format, ...) __dtx_log(__current_file_log, OS_LOG_TYPE_ERROR, __current_log_prefix, format, ##__VA_ARGS__)
-#define dtx_log_fault(format, ...) __dtx_log(__current_file_log, OS_LOG_TYPE_FAULT, __current_log_prefix, format, ##__VA_ARGS__)
+#define dtx_log_debug(format, ...) __dtx_log(__prepare_and_return_file_log(), OS_LOG_TYPE_DEBUG, __current_log_prefix, format, ##__VA_ARGS__)
+#define dtx_log_info(format, ...) __dtx_log(__prepare_and_return_file_log(), OS_LOG_TYPE_INFO, __current_log_prefix, format, ##__VA_ARGS__)
+#define dtx_log_error(format, ...) __dtx_log(__prepare_and_return_file_log(), OS_LOG_TYPE_ERROR, __current_log_prefix, format, ##__VA_ARGS__)
+#define dtx_log_fault(format, ...) __dtx_log(__prepare_and_return_file_log(), OS_LOG_TYPE_FAULT, __current_log_prefix, format, ##__VA_ARGS__)
 
 extern
 __attribute__((__not_tail_called__))
