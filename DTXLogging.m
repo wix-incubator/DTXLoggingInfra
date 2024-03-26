c8f766fa1762e83ee3763ed7135be5c1dfa2df2d
Asaf Korem <asaf.korem@gmail.com>
2024-03-27 00:50:12 +0200

    DTXLogging.m: log long messages in parts, avoid truncation.

--- DTXLogging.m
+++ DTXLogging.m
@@ -36,7 +36,39 @@ void __dtx_logv(os_log_t log, os_log_type_t logType, NSString* prefix, NSString*
 		return;
 	}
 	
-	NSString* message = [[NSString alloc] initWithFormat:format arguments:args];
-	
-	os_log_with_type(log, logType, "%{public}s%{public}s", prefix.UTF8String, message.UTF8String);
+	NSString *message = [[NSString alloc] initWithFormat:format arguments:args];
+
+	// Split the message into parts if it's longer than this threshold
+	const NSUInteger maxLength = 512; // based on truncation behavior
+	if (message.length > maxLength) {
+		NSMutableArray *parts = [NSMutableArray array];
+
+		for (NSUInteger startIndex = 0; startIndex < message.length; startIndex += maxLength) {
+			NSRange range = NSMakeRange(startIndex, MIN(maxLength, message.length - startIndex));
+			NSString *part = [message substringWithRange:range];
+			[parts addObject:part];
+		}
+
+		// Message about splitting the message
+		os_log_with_type(
+			log,
+			logType,
+			"Message was too long and was split into %{public}lu parts",
+			(unsigned long)parts.count
+		);
+
+		[parts enumerateObjectsUsingBlock:^(NSString *part, NSUInteger idx, BOOL *stop) {
+			os_log_with_type(
+				log,
+				logType,
+				"Part %{public}lu/%{public}lu: %{public}s%{public}s",
+				(unsigned long)(idx + 1),
+				(unsigned long)parts.count,
+				[prefix UTF8String],
+				[part UTF8String]
+			);
+		}];
+	} else {
+		os_log_with_type(log, logType, "%{public}s%{public}s", [prefix UTF8String], [message UTF8String]);
+	}
 }
